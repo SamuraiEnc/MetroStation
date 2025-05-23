b73d68987391a2b856e1cece10dd4ec06defeba0
SamuraiEnc <egorcepeleve@gmail.com>
2025-05-23 22:53:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const clickButton = document.querySelector('.bt-click');
+const txtPointer = document.querySelector('.pointer');
+let counter = 0;
 
 // Для touch-устройств
 clickButton.addEventListener('touchstart', function(e) {
@@ -28,3 +30,19 @@ clickButton.addEventListener('mouseleave', function() {
     this.style.transform = 'scale(1)';
     this.style.boxShadow = '0 0 20px rgba(255, 87, 34, 0.5)';
 });
+
+clickButton.addEventListener('click', function(){
+    counter+=200;
+    console.log(counter);
+    txtPointer.textContent = formatNumber(counter);
+});
+
+function formatNumber(num) {
+    if (num >= 1000000) {
+        return (num / 1000000).toFixed(1) + 'm'; // 1.2m
+    }
+    if (num >= 1000) {
+        return (num / 1000).toFixed(1) + 'k'; // 1.5k
+    }
+    return num.toString(); // Меньше 1000 - выводим как есть
+}
